64086d980e40b7f7c6c938f35e2242f3eec833ee
Andrzej Brozyniak <Andrzej.Brozyniak.ext@direkt-gruppe.de>
2021-08-05 16:15:50 +0200

    Initial commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+resource "azurerm_mysql_server" "server" {
+  name                = "example-mysqlserver"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  administrator_login          = var.admin_login
+  administrator_login_password = var.admin_password
+
+  sku_name   = var.sku 
+  storage_mb = var.storage 
+  version    = var.version 
+
+  auto_grow_enabled                 = var.auto_grow 
+  backup_retention_days             = var.retention_days 
+  geo_redundant_backup_enabled      = var.redundancy 
+  infrastructure_encryption_enabled = var.encryption 
+  public_network_access_enabled     = var.public_net_access 
+  ssl_enforcement_enabled           = var.ssl 
+  ssl_minimal_tls_version_enforced  = var.ssl_tls_version 
+  tags                              - var.tags
+}
---
+++ variables.tf
@@ -0,0 +1,63 @@
+variable admin_login {
+    type        = string
+}
+   
+variable admin_password {
+    type        = string
+    
+}
+
+variable sku {
+    type        = string
+    default     = "B_Gen5_2"
+}
+variable storage {
+    type        = number
+    default     = 5120
+}
+
+variable version {
+    type        = number
+    default     = "5.7"
+}
+
+variable auto_grow {
+    type        = bool
+    default     = true
+}
+
+variable retention_days {
+    type        = number
+    default     = 7
+}
+
+variable redundancy {
+    type        = bool
+    default     = false
+}
+
+variable encryption {
+    type        = bool
+    default     = false
+}
+
+variable public_net_access {
+    type        = bool
+    default     = true
+}
+
+variable ssl {
+    type        = bool
+    default     = true
+}
+
+variable "tags" {
+    description = "A map of tags to set on every taggable resources. Empty by default."
+    type        = map(string)
+    default     = {}
+}
+
+variable ssl_tls_version {
+    type        = string
+    default     = "TLS1_2"
+}
